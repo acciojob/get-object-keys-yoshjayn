8773427bdd5854cca6f6a7e0e45b6b448420f9c5
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-04-08 16:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function getKeys(obj) {
   return Object.keys(obj);
 }
 
+//for passing test cases
+// Attach getKeys function to Object prototype
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+
 // Testing the function
 console.log(getKeys(student)); // Output: ["name"]
 
